d35dc5dcadfea0ce1460a52a14c0ab68a02cab38
kenta_mitsui <kentam12@gmail.com>
2022-10-25 21:38:55 +0900

    feat: 機能の作成等 #3

--- .github/ISSUE_TEMPLATE/feature.md
+++ .github/ISSUE_TEMPLATE/feature.md
@@ -4,30 +4,35 @@ about: File a feature/issue 新規の機能・タスクの割当
 title: '[Feat] <title>'
 labels: Enhancement
 assignees: ''
-
 ---
 
-
 ### 概要
+
 - 内容のサマリ
 
 ### 内容の詳細
+
 - 対応内容の詳細
 
 ### チェックリスト
- 
+
 ### 関連リンク
+
 - 参考情報のリンクなど
+- `https://www.webtips.dev/tic-tac-toe-in-javascript`
+- `https://www.sourcecodeexamples.net/2020/09/javascript-tic-tac-toe-game.html`
 
 ### そのほか
+
 - 特記事項
 
-***
-  
+---
+
 🔔 Note:
+
 - 対応時は　`$ git pull origin develop` で最新の情報を取得してください
 - `$ git switch -c "新規ブランチ名" `でブランチを作成し、その中で更新してください
-- 対応内容が終わったらPullRequestをたててください
-- 迷ったらDiscordで相談しましょう！
+- 対応内容が終わったら PullRequest をたててください
+- 迷ったら Discord で相談しましょう！
 
 <!-- A concise description of what you're experiencing. -->
--- main.js
+++ main.js
@@ -1,4 +1,97 @@
-const main = () => {
-	const target = document.getElementById("target");
-};
-main();
+'use strict'
+
+const playerTurnText = document.getElementById('player-turn')
+const oPlayer = (playerTurnText.textContent = 'O turn')
+
+const gameObject = {
+  oTurn: true,
+  turnCount: 0,
+  oState: [],
+  xState: [],
+  WINNING_CONDITION: [
+    // 列
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // 行
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // 斜め
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+}
+
+// 引数(下記では'turnCount'を使用)の剰余によって、
+// player表示を切り替える
+function switchPlayer(e) {
+  e % 2 === 0
+    ? (document.querySelector('#player-turn').textContent = 'X turn')
+    : (document.querySelector('#player-turn').textContent = 'O turn')
+}
+
+// 真偽値により、playerturnを判定する
+function switchTurn() {
+  gameObject.oTurn = !gameObject.oTurn
+}
+
+function displayWinMessage(oInfo, xInfo) {
+  if (oInfo || xInfo) {
+    document
+      .querySelectorAll('.board-cell')
+      .forEach((boardCell) => boardCell.classList.add('clicked'))
+    document.querySelector('.finish').classList.add('visible')
+    document.querySelector('.finish-text').textContent = oInfo
+      ? 'Oの勝利'
+      : 'Xの勝利'
+  }
+}
+
+// クリック時のイベント処理
+document.addEventListener('click', (e) => {
+  const target = e.target
+  // ↓はHTMLのboard-cellクラス名の有無を確認
+  // クリック時にクラスが有れば'true'、無ければ'false'を返す
+  const boardCell = target.classList.contains('board-cell')
+  const click = target.classList.contains('clicked')
+  const boardCellIndex = target.dataset.index
+  const addClicked = target.classList.add('clicked')
+  const addState = target.classList.add(gameObject.oTurn ? 'o' : 'x')
+
+  if (boardCell && !click) {
+    //両playerへの配列格納時に、HTMLのindexnumberを数値に変換する
+    gameObject.oTurn
+      ? gameObject.oState.push(Number(boardCellIndex))
+      : gameObject.xState.push(Number(boardCellIndex))
+
+    addState
+    addClicked
+    switchTurn()
+    switchPlayer(gameObject.turnCount)
+    // クリックしたらカウントを1ずつ増加
+    gameObject.turnCount++
+
+    // 全ての目が埋まった場合、引き分けと表示する処理
+    if (!document.querySelectorAll('.board-cell:not(.clicked)').length) {
+      document.querySelector('.finish').classList.add('visible')
+      document.querySelector('.finish-text').textContent = '引き分け'
+    }
+
+    // 勝利判定の処理
+    gameObject.WINNING_CONDITION.forEach((WINNING_CONDITION) => {
+      // everyメソッドは、列内のすべての要素が指定された関数で
+      // 実装されたテストに合格するかどうかを確認する
+      const winSenkou = WINNING_CONDITION.every((state) =>
+        gameObject.oState.includes(state)
+      )
+      const winKoukou = WINNING_CONDITION.every((state) =>
+        gameObject.xState.includes(state)
+      )
+
+      // どちらかの勝利が判定された時点で、全てのboardcellをクリック済にし
+      // 勝利時のテキストを真偽値を用いて表示する
+      displayWinMessage(winSenkou, winKoukou)
+    })
+  }
+})
